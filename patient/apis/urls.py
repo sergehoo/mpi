cf9b238e19713c158648eb3d446019f8285026dd
ogahsergesergehoo <sergeleo14@yahoo.fr>
2025-03-31 11:53:34 +0000

    lo

--- patient/apis/urls.py
+++ patient/apis/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
@@ -8,5 +10,9 @@ router.register(r'patients', PatientViewSet)
 router.register(r'platforms', PlatformViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
-]
+                  path('', include(router.urls)),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
