a1dc4a8e119cc83912eb54055bb9fa072b60beb0
ogahsergesergehoo <sergeleo14@yahoo.fr>
2025-03-31 11:50:23 +0000

    dr

--- mpi/urls.py
+++ mpi/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
@@ -23,15 +25,19 @@ from patient.apis.views import PlatformTokenView
 from patient.views import Landing
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('apis-auth/', include('rest_framework.urls')),
-    path('mpi/', include('patient.apis.urls')),
+                  path('admin/', admin.site.urls),
+                  path('apis-auth/', include('rest_framework.urls')),
+                  path('mpi/', include('patient.apis.urls')),
 
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('platform/token/', PlatformTokenView.as_view(), name='platform_token'),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('', Landing.as_view(), name='landing')
+                  path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+                  path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+                  path('platform/token/', PlatformTokenView.as_view(), name='platform_token'),
+                  path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+                  path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+                  path('', Landing.as_view(), name='landing')
 
-]
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
